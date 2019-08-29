243adea90bb053d7316a223a9394eea01377d5d3
DenisPitsul <denia.pitsul@gmail.com>
2019-08-29 13:32:15 +0300

    merge

--- src/app/system/system-routing.module.ts
+++ src/app/system/system-routing.module.ts
@@ -2,9 +2,17 @@ import {SystemComponent} from './system.component';
 import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
 import {AuthGuard} from '../auth/auth.guard';
+import {UsersComponent} from './users/users.component';
+import {ControlWorksComponent} from './control-works/control-works.component';
+import {GroupsComponent} from './groups/groups.component';
 
 const routes: Routes = [
-  {path: '', component: SystemComponent}
+  {path: '', component: SystemComponent, children: [
+    {path: 'users', component: UsersComponent},
+    {path: 'control_works', component: ControlWorksComponent},
+    {path: 'groups', component: GroupsComponent}
+    ]
+  }
 ];
 
 @NgModule({
